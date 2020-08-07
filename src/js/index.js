426edd64efcb2cb3cdfb4adccd6261a8f108b4a7
William Barela <william.barela.dev@gmail.com>
2020-08-07 00:01:56 -0500

    add baseURI to index.js to reduce obstrufication

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,9 @@ import { Adapter } from './Adapter.js';
 import { Seminar } from './Seminar.js';
 
 const date = new Date();
-const adapter = new Adapter(date);
+const baseURI = "https://raw.githubusercontent.com/ttu-biology/seminar-dept/master/seminars/";
+
+const adapter = new Adapter(date, baseURI);
 const seminarPage = new Seminar(adapter);
 
 let x = document.getElementById("seminars");
