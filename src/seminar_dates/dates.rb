22a3f8611521d7574a8538e8212fecbb1d7af1e6
William Barela <william.barela.dev@gmail.com>
2020-08-07 12:35:51 -0500

    generalize dates.rb to easily create semester dates

--- src/seminar_dates/dates.rb
+++ src/seminar_dates/dates.rb
@@ -1,17 +1,19 @@
 require 'date'
 
-fall_2019_start = Date.new(2019,8,26)
-fall_2019_end = Date.new(2019,12,4)
+academic_year = 2020
 
-spring_2020_start = Date.new(2020,1,15)
-spring_2020_end = Date.new(2020,5,5)
+fall_start = Date.new(academic_year,8,26)
+fall_end = Date.new(academic_year,12,4)
+
+spring_start = Date.new(academic_year + 1,1,15)
+spring_end = Date.new(academic_year + 1,5,5)
  
  
-fall_2019_wednesdays = (fall_2019_start..fall_2019_end).to_a.select{|k| k.wday == 3}
-spring_2020_wednesdays = (spring_2020_start..spring_2020_end).to_a.select{|k| k.wday == 3}
+fall_wednesdays = (fall_start..fall_end).to_a.select{|k| k.wday == 3}
+spring_wednesdays = (spring_start..spring_end).to_a.select{|k| k.wday == 3}
  
-puts "Fall 2019"
-fall_2019_wednesdays.map{|date| puts date.strftime('%B %d, %Y')}
+puts "Fall #{academic_year}"
+fall_wednesdays.map{|date| puts date.strftime('%B %d, %Y')}
  
-puts "\nSpring 2020"
-spring_2020_wednesdays.map{|date| puts date.strftime('%B %d, %Y')}
+puts "\nSpring #{academic_year + 1}"
+spring_wednesdays.map{|date| puts date.strftime('%B %d, %Y')}
