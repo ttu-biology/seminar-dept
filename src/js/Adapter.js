5813a358807a4347fb3d9a9a25fb969b32843d61
William Barela <william.barela.dev@gmail.com>
2020-08-06 23:54:13 -0500

    add methods to adapter to construct past, present and future seminar URI

--- src/js/Adapter.js
+++ src/js/Adapter.js
@@ -1,4 +1,37 @@
 export class Adapter {
-  constructor(date){
+  constructor(date, baseURI="https://raw.githubusercontent.com/ttu-biology/seminar-dept/master/seminars/"){
+    this.currentYear = date.getFullYear();
+    this.currentMonth = date.getMonth();
+    this.pastYear = this.currentYear - 1;
+    this.futureYear = this.currentYear + 1;
+    this.baseURI = baseURI;
+  }
+
+  get currentSemester() {
+    return (this.currentMonth <= 4) ? "spring" : "fall"
+  }
+
+  get nonCurrentSemester() {
+    return (this.currentSemester === "spring") ? "fall" : "spring"
+  }
+
+  get currentSemesterURI() {
+    return `${this.baseURI}${this.currentYear}_${this.currentSemester}.yml`
+  }
+
+  get pastSemesterURI() {
+    if (this.currentSemester == "spring") {
+      return `${this.baseURI}${this.PastYear}_${this.nonCurrentSemester}.yml`
+    } else {
+      return `${this.baseURI}${this.currentYear}_${this.nonCurrentSemester}.yml`
+    }
+  }
+
+  get futureSemesterURI() {
+    if (this.currentSemester == "fall") {
+      return `${this.baseURI}${this.futureYear}_${this.nonCurrentSemester}.yml`
+    } else {
+      return `${this.baseURI}${this.currentYear}_${this.nonCurrentSemester}.yml`
+    }
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,11 @@ import { Seminar } from './Seminar.js';
 const date = new Date();
 const adapter = new Adapter(date);
 const seminarPage = new Seminar(adapter);
+
+let x = document.getElementById("seminars");
+x.innerHTML = `<ul>
+                <li>past: ${adapter.pastSemesterURI}</li>
+                <li>current: ${adapter.currentSemesterURI}</li>
+                <li>future: ${adapter.futureSemesterURI}</li>
+               </ul> 
+                `;
